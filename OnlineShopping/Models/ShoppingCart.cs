fa37eb2b47abb489cd66c08befc20b27bfbb549c
Filmon Fitwi <filmon.kidane08@gmail.com>
2024-03-19 22:45:01 -0500

    Add project files.

---
+++ OnlineShopping/Models/ShoppingCart.cs
@@ -0,0 +1,20 @@
+﻿namespace OnlineShopping.Models
+{
+    public class ShoppingCartItem
+    {
+        public int ProductId { get; set; }
+        public string Quantity { get; set; }
+
+        public ShoppingCartItem(int productId, string quantity)
+        {
+            ProductId = productId;
+            Quantity = quantity;
+        }
+    }
+
+
+    public class ShoppingCart
+    {
+        public List<ShoppingCartItem>? Items { get;set; }
+    }
+}
---
+++ OnlineShopping/Models/User.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineShopping.Models
+{
+    public class User
+    {
+  
+            public int Id { get; set; }
+
+            [Required(ErrorMessage = "Please enter your username.")]
+            public string? UserName { get; set; }
+
+            [Required(ErrorMessage = "Please enter your email address.")]
+
+            [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+            public string Email { get; set; }
+
+            [Required(ErrorMessage = "Please enter your password.")]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            public string Name { get; set; }
+            public string Address {  get; set; }
+            public string Phone { get; set; }
+
+        public User(int id, string userName, string email, string password, string name, string address, string phone)
+        {
+            Id = id;
+            Email = email;
+            UserName = userName;
+            Email = email;
+            Password= password;
+            Name = name;
+            Address = address;
+            Phone = phone;
+
+        }
+           
+            }
+}
